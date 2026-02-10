78bd248853ffb2e0a5d95d0d92f29e6a2de30cb0
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2026-02-10 13:01:36 +0530

    js

---
+++ js-lecture-3/lecture-3.js
@@ -0,0 +1,24 @@
+// promt se tum jo bhi loge vo string ban jayega
+// javascript me arrays ka type object hota hai 
+
+// console.error("error ala re")
+
+// let a = prompt("name ?");
+
+// alert(`hey ${a}`);
+
+let msg = "I love darshan";
+ msg.slice(2,6);
+
+//  console.log(msg.slice())
+
+// let name = "darshan";
+//  console.log(`hey ${name} welcome to js`)
+
+let a =  (5 + 10) * 2;
+console.log(a)
+
+let as = "darshan";
+as = true;
+
+console.log(typeof as);
