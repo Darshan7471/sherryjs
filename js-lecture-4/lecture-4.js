c6ef5ab586b9a1af1e79fe073a6bf0bafe74805e
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2026-02-10 15:34:31 +0530

    js

---
+++ js-lecture-4/lecture-4.js
@@ -0,0 +1,16 @@
+// operators in js
+
+// arithmatic operaters in js
+
+// + - * % **
+
+12 + 13
+12 - 13
+12 * 13
+12 % 13
+12 ** 13
+
+
+// assignement operaters in js
+
+121 > 13 ? console.log("sahi hai bhai") : console.log("galat hai bhai");
